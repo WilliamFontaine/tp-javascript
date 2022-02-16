a3edd9580656ad23a26e1dcd23733472e19a16a4
WilliamFontaine <william.fontaine0308@gmail.com>
2022-02-16 17:43:10 +0100

    ajout tp3

---
+++ tp3/dist/js/main.js
@@ -0,0 +1,38 @@
+iterate();
+
+let h1 = document.querySelector('h1');
+logMessageWithDate(h1.innerHTML);
+
+let titleNews = document.querySelector('#titleNews');
+logMessageWithDate(titleNews.innerHTML);
+
+let titles = document.querySelectorAll('.title');
+titles.forEach(element => logMessageWithDate(element.innerHTML));
+
+let button = document.querySelector('input[name="addNewsBtn"]');
+bindButton(button);
+
+let buttons = document.querySelectorAll('article button');
+buttons.forEach(function(element) {
+    element.onclick = viewdetailClick;
+});
+
+let articles = JSON.parse(ALLNEWSJSON);
+articles.forEach(function(element) {
+    console.log(element);
+
+    try {
+        let a = new Article(element.id, element.title, element.description);
+        a.insertArticleHtml();
+    } catch (e) {
+        clearErrors();
+        let form = document.querySelector('#addNewsForm');
+
+        if (e instanceof RequiredPropertyError || e instanceof DuplicateArticleError) {
+            addError(e.message, form);
+        } else {
+            console.error(e);
+            addError('Une erreur inconnue est survenue !', form);
+        }
+    }
+});
